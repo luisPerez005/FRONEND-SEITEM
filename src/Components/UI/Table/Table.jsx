f0273422ee9b23841f39f181db785c81f2e592ea
luisPerez005 <perezperezluis005@gmail.com>
2025-05-26 10:08:16 -0500

    page-garantias

--- src/Components/UI/Table/Table.jsx
+++ src/Components/UI/Table/Table.jsx
@@ -1,40 +1,9 @@
 import React from 'react'
 import './Table.css'
-export const Table = () => {
+export const Table = ({children}) => {
   return (
      <table className='table'>
-      <thead>
-        <tr>
-          <th>TORRE TINY</th>
-          <th>TORRE GRANDE</th>
-          <th>MONITOR</th>
-          <th>LECTOR BIDIRECCIONAL</th>
-          <th>LECTOR OMNIDIRECCIONAL</th>
-          <th>IMPRESORA USB</th>
-          <th>IMPRESORA RED</th>
-          <th>TECLADO</th>
-          <th>MOUSE</th>
-          <th>IMPRESORA ETIQUETAS</th>
-          <th>BALANZA</th>
-          <th>FECHA INVENTARIO</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>17</td>
-          <td>4</td>
-          <td>16</td>
-          <td>4</td>
-          <td>0</td>
-          <td>2</td>
-          <td>1</td>
-          <td>17</td>
-          <td>17</td>
-          <td>1</td>
-          <td>1</td>
-          <td>FECHA</td>
-        </tr>
-      </tbody>
+      {children}
     </table>
   )
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { EstructurePage } from '../../Components/Layouts/EstructurePage/EstructurePage'
+import { ColumTable } from '../../Components/UI/ColumTable/ColumTable'
+import { FileTable } from '../../Components/UI/FileTable/FileTable'
 import { Table } from '../../Components/UI/Table/Table'
 export const Home = () => {
   return (
@@ -9,7 +11,40 @@ export const Home = () => {
         <button>AGREGAR NUEVOS EQUIPOS</button>
       </div>
       <div>
-        <Table />
+        <Table> 
+          <ColumTable>
+             <tr>
+          <th>TORRE TINY</th>
+          <th>TORRE GRANDE</th>
+          <th>MONITOR</th>
+          <th>LECTOR BIDIRECCIONAL</th>
+          <th>LECTOR OMNIDIRECCIONAL</th>
+          <th>IMPRESORA USB</th>
+          <th>IMPRESORA RED</th>
+          <th>TECLADO</th>
+          <th>MOUSE</th>
+          <th>IMPRESORA ETIQUETAS</th>
+          <th>BALANZA</th>
+          <th>FECHA INVENTARIO</th>
+        </tr>
+          </ColumTable>
+          <FileTable>
+             <tr>
+          <td>17</td>
+          <td>4</td>
+          <td>16</td>
+          <td>4</td>
+          <td>0</td>
+          <td>2</td>
+          <td>1</td>
+          <td>17</td>
+          <td>17</td>
+          <td>1</td>
+          <td>1</td>
+          <td>FECHA</td>
+        </tr>
+          </FileTable>
+        </Table>
       </div>
         
       </EstructurePage>
