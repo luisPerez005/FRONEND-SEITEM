67d10606d17d486b8d366358b31f7be7e4fb4257
luisPerez005 <bermudezluisperezangel@gmail.com>
2025-05-29 13:00:03 -0500

    v2

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,55 +1,62 @@
-import React from 'react'
-import { EstructurePage } from '../../Components/Layouts/EstructurePage/EstructurePage'
-import { ColumTable } from '../../Components/UI/ColumTable/ColumTable'
-import { FileTable } from '../../Components/UI/FileTable/FileTable'
-import { Table } from '../../Components/UI/Table/Table'
+import React from "react";
+import { useState } from "react";
+import { EstructurePage } from "../../Components/Layouts/EstructurePage/EstructurePage";
+import { ColumTable } from "../../Components/UI/ColumTable/ColumTable";
+import { FileTable } from "../../Components/UI/FileTable/FileTable";
+import { Table } from "../../Components/UI/Table/Table";
+import { Form } from "../../Components/UI/Form/Form";
 export const Home = () => {
+  const [formActivate, setFormActivate] = useState(false);
+
   return (
     <>
-    <EstructurePage>
-      <div>
-        <button>AGREGAR NUEVOS EQUIPOS</button>
-      </div>
-      <div>
-        <Table> 
-          <ColumTable>
-             <tr>
-          <th>TORRE TINY</th>
-          <th>TORRE GRANDE</th>
-          <th>MONITOR</th>
-          <th>LECTOR BIDIRECCIONAL</th>
-          <th>LECTOR OMNIDIRECCIONAL</th>
-          <th>IMPRESORA USB</th>
-          <th>IMPRESORA RED</th>
-          <th>TECLADO</th>
-          <th>MOUSE</th>
-          <th>IMPRESORA ETIQUETAS</th>
-          <th>BALANZA</th>
-          <th>FECHA INVENTARIO</th>
-        </tr>
-          </ColumTable>
-          <FileTable>
-             <tr>
-          <td>17</td>
-          <td>4</td>
-          <td>16</td>
-          <td>4</td>
-          <td>0</td>
-          <td>2</td>
-          <td>1</td>
-          <td>17</td>
-          <td>17</td>
-          <td>1</td>
-          <td>1</td>
-          <td>FECHA</td>
-        </tr>
-          </FileTable>
-        </Table>
-      </div>
-        
+      <EstructurePage>
+        <div>
+          <button onClick={() => setFormActivate(true)}>
+            AGREGAR NUEVOS EQUIPOS
+          </button>
+        </div>
+        <div>
+          {formActivate ? (
+            <Form />
+          ) : (
+            <Table>
+              <ColumTable>
+                <tr>
+                  <th>TORRE TINY</th>
+                  <th>TORRE GRANDE</th>
+                  <th>MONITOR</th>
+                  <th>LECTOR BIDIRECCIONAL</th>
+                  <th>LECTOR OMNIDIRECCIONAL</th>
+                  <th>IMPRESORA USB</th>
+                  <th>IMPRESORA RED</th>
+                  <th>TECLADO</th>
+                  <th>MOUSE</th>
+                  <th>IMPRESORA ETIQUETAS</th>
+                  <th>BALANZA</th>
+                  <th>FECHA INVENTARIO</th>
+                </tr>
+              </ColumTable>
+              <FileTable>
+                <tr>
+                  <td>17</td>
+                  <td>4</td>
+                  <td>16</td>
+                  <td>4</td>
+                  <td>0</td>
+                  <td>2</td>
+                  <td>1</td>
+                  <td>17</td>
+                  <td>17</td>
+                  <td>1</td>
+                  <td>1</td>
+                  <td>FECHA</td>
+                </tr>
+              </FileTable>
+            </Table>
+          )}
+        </div>
       </EstructurePage>
     </>
-  )
-}
-
-
+  );
+};
--- src/Pages/Prestamos/Prestamos.jsx
+++ src/Pages/Prestamos/Prestamos.jsx
@@ -1,7 +1,52 @@
 import React from 'react'
+import { EstructurePage } from '../../Components/Layouts/EstructurePage/EstructurePage'
+import {Table} from '../../Components/UI/Table/Table'
+import { ColumTable } from '../../Components/UI/ColumTable/ColumTable'
+import { FileTable } from '../../Components/UI/FileTable/FileTable'
 
 export const Prestamos = () => {
   return (
-    <div>Prestamos</div>
+    <EstructurePage>
+      <div>
+              <button>AGREGAR PRESTAMO</button>
+            </div>
+            <div>
+              <Table>
+                <ColumTable>
+                  <tr>
+                    <th>NOMBRE</th>
+                    <th>EQUIPO</th>
+                    <th>CANTIDAD EQUIPOS</th>
+                    <th>FECHA DE PRESTAMO</th>
+                    <th>ESTADO</th>
+                    <th>DESCRIPCION</th>
+                  </tr>
+                </ColumTable>
+                <FileTable>
+                  <tr>
+                    <td>JHON</td>
+                    <td>
+                      <select id="opciones" name="opciones">
+                        <option value="opcion1">IMPRESORA ETIQUETAS</option>
+                        <option value="opcion2">IMPRESORA USB O RED</option>
+                        <option value="opcion3">BALANZA</option>
+                      </select>
+                    </td>
+                    <td>1</td>
+                    <td>febrero 2025</td>
+                    <td>
+                      <select id="opciones" name="opciones">
+                        <option value="opcion1">EN PRESTAMO</option>
+                        <option value="opcion2">ENTREGADO</option>
+                      </select>
+                    </td>
+                    <td>
+                      La impresora se encuentra en garantia por un daño en la board
+                    </td>
+                  </tr>
+                </FileTable>
+              </Table>
+            </div>
+    </EstructurePage>
   )
 }
